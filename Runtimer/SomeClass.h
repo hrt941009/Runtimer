79bc1ba05a939c736a09ee400997882873e45199
hrt941009 <13857276@qq.com>
2015-08-05 17:41:34 +0800

    class add method

---
+++ Runtimer/SomeClass.h
@@ -0,0 +1,15 @@
+//
+//  SomeClass.h
+//  Runtimer
+//
+//  Created by henyep on 15/8/5.
+//  Copyright (c) 2015年 com. All rights reserved.
+//
+#import <objc/runtime.h>
+#import <Foundation/Foundation.h>
+
+@interface SomeClass : NSObject
+{
+    NSString *_privateName;
+}
+@end
---
+++ Runtimer/SomeClass.m
@@ -0,0 +1,17 @@
+//
+//  SomeClass.m
+//  Runtimer
+//
+//  Created by henyep on 15/8/5.
+//  Copyright (c) 2015年 com. All rights reserved.
+//
+
+#import "SomeClass.h"
+
+@implementation SomeClass
+- (id)init {
+    self = [super init];
+    if (self) _privateName = @"Steve";
+    return self;
+}
+@end
